45c6d0b62558c582e7db7dc934fb8f009e82c480
walter Rodriguez <walter.rodriguez1992@gmail.com>
2022-09-07 13:43:50 -0500

    version 1.0

--- premiosplatziapp/polls/models.py
+++ premiosplatziapp/polls/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
-# Create your models here.
+class Question(models.Model):
+    #id django lo crea automaticamente y de manera incremental
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+
+    #funcion que retornara el texto del objeto
+    def __str__(self):
+        return self.question_text
+
+    #funciona que permite ver si una pregunta tiene una fecha reciente de publicacion
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
+
+
